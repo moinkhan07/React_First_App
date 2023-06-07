b28f9cc6a9a290efffb1e950e7b780330b1a9f45
Moin Khan <moin6k@gmail.com>
2023-06-07 16:04:34 +0530

    First react application added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import Header from './components/Header';
 import Movie from './components/Movie';
-import movie from './movie.json'
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 
 function App() {
+
+  let [data,setData] = useState([]);
+
+  useEffect(() => {
+    let fetchData = async ()=>{
+      let res = await fetch("https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies");
+      let movie = await res.json();
+      setData(movie);
+    }
+    fetchData();
+  })
+  
 
   const [num,setNum] = useState(1);
 
@@ -25,7 +35,7 @@ function App() {
       <h1 className='pj'>Project 2 using props</h1>
       <div id='main'>
         {
-        movie.map((el,i)=>{
+        data.map((el,i)=>{
           return(
           <Movie key={i} title={el.Title} img={el.Poster} year={el.Year} runtime={el.Runtime}/>
           )
